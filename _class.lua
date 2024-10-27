1488ce53e5ce5a17669cd82eeaa7443845a8ca96
vaxherd <111935183+vaxherd@users.noreply.github.com>
2024-10-28 08:36:38 +0900

    Import updated class implementation from WoWXIV

--- _class.lua
+++ _class.lua
@@ -60,11 +60,10 @@ Multiple inheritance is not supported.
 A constructor may be provided in the class definition by declaring a
 method named "__constructor":
 
-    MyClass = class({
-        __constructor = function(self, arg)
-            self.value = arg
-        end
-    })
+    MyClass = class()
+    function MyClass:__constructor(arg)
+        self.value = arg
+    end
 
     instance = MyClass(123)
     print(instance.value)  -- prints "123"
@@ -80,6 +79,51 @@ __super() raises an error.)
 Note that __super() is _only_ valid in constructors; instance methods
 must explicitly name the parent class when calling overridden methods.
 
+
+Additionally, classes may provide a class method named "__allocator" which
+creates the initial table for an instance:
+
+    MyClass = class()
+    MyClass.singleton = {}
+    function MyClass.__allocator(class)
+        return class.singleton
+    end
+
+    instance = MyClass()
+    print(instance == MyClass.singleton)  -- prints "true"
+
+This can be useful for wrapping external objects in classes where the
+instance must use an externally provided table, such as Frame objects
+in the game World of Warcraft.  If any constructor arguments are passed
+to the instance creation call, they will be passed to the allocator
+method as well.
+
+The allocator must return a table value (it is not allowed to fail).
+The method should raise an error under any condition which would prevent
+it from creating a new instance.
+
+If the allocator sets a metatable on the returned instance with an
+"__index" field, the value must be either a function or a table; the
+value will effectively be inserted between the instance table itself and
+the class definition when performing a member lookup.
+
+__super() is not supported in allocators; an overriding allocator wishing
+to call its base class's implementation must explicitly name the base
+class:
+
+    MyClass = class()
+    function MyClass.__allocator(class, ...)
+        local instance = {}
+        -- ...
+        return instance
+    end
+    MySubClass = class(MyClass)
+    function MySubClass.__allocator(class, ...)
+        local instance = MyClass:__allocator(...)
+        -- ...
+        return instance
+    end
+
 ]]
 
 ------------------------------------------------------------------------
@@ -89,31 +133,80 @@ must explicitly name the parent class when calling overridden methods.
 local _, module = ...
 module = module or {} -- so the file can also be loaded with a simple require()
 
+-- Error messages are defined as constants for testing convenience.
+local parent_type_error_msg = "Parent class must be a table"
 local super_error_msg = "__super() called from class with no superclass"
-local function super_error()
-    error(super_error_msg)
+local constr_type_error_msg = "__constructor must be a function"
+local alloc_result_error_msg = "__allocator() must return a table"
+
+local function call_super(...)
+    local super = getmetatable(getfenv(2)).parent
+    if super then
+        super.__constructor(...)
+    else
+        error(super_error_msg)
+    end
 end
 
 function module.class(parent)
-    local classdef = {}
+    if parent and type(parent) ~= "table" then
+        error(parent_type_error_msg)
+    end
+    local classdef = {__super = call_super}
     local instance_metatable = {__index = classdef}
-    local class_metatable = {
-        __call = function(thisclass, ...)
-            local instance = setmetatable({}, instance_metatable)
-            instance:__constructor(...)
-            return instance
+    local class_metatable = {}
+    class_metatable.__call = function(thisclass, ...)
+        local instance
+        if thisclass.__allocator then
+            instance = thisclass.__allocator(thisclass, ...)
+            assert(type(instance) == "table", alloc_result_error_msg)
+            local metatable = getmetatable(instance) or {}
+            -- Avoid directly referencing classdef for consistency.
+            metatable.__index = metatable.__index or instance_metatable.__index
+            setmetatable(instance, metatable)
+        else
+            instance = setmetatable({}, instance_metatable)
         end
-    }
-    if parent then
-        classdef.__super = function(...) parent.__constructor(...) end
-        class_metatable.__index = parent
-    else
-        classdef.__super = super_error
-        -- Define a default constructor for base classes so the "new"
-        -- operation doesn't need to check for its presence.
-        classdef.__constructor = function() end
+        instance:__constructor(...)
+        return instance
+    end
+    -- Hide the constructor so we can attach the parent class to its
+    -- definition when (if) it is later declared.
+    class_metatable.__newindex = function(t, k, v)
+        if k == "__constructor" then
+            assert(t == classdef)
+            if type(v) ~= "function" then
+                error(constr_type_error_msg)
+            end
+            -- We need to attach the parent class to the function
+            -- definition itself, since each constructor in an inheritance
+            -- chain needs to know its own parent and we can only store a
+            -- single parent in the instance table.  Rather than storing
+            -- the parent in a function-local variable which could
+            -- potentially interfere with the constructor code itself, we
+            -- put it in a metatable entry which is much less likely to
+            -- cause a conflict.
+            setfenv(v, setmetatable({}, {__index=getfenv(v), parent=parent}))
+            class_metatable.constructor = v
+        else
+            rawset(t, k, v)
+        end
+    end
+    class_metatable.__index = function(t, k)
+        if k == "__constructor" then
+            return class_metatable.constructor
+        else
+            return parent and parent[k]
+        end
     end
     setmetatable(classdef, class_metatable)
+    -- Define a default constructor so the "new" operation doesn't need to
+    -- check for its presence.
+    if parent then
+        classdef.__constructor = function(self,...) self:__super(...) end
+    else
+        classdef.__constructor = function() end
+    end
     return classdef
 end
 
@@ -195,6 +288,16 @@ local tests = {
         assert(instance.x == 70)
     end,
 
+    InvalidConstructorType = function()
+        local Class = class()
+        local function f()
+            Class.__constructor = "foo"
+        end
+        local result, errmsg = pcall(f)
+        assert(result == false)
+        assert(errmsg:find(constr_type_error_msg, 1, true))
+    end,
+
     ConstructorArgs = function()
         local Class = class()
         function Class:__constructor(a, b, c)
@@ -202,6 +305,119 @@ local tests = {
         end
         local instance = Class(1, 2, 3)
         assert(instance.x == 123)
+    end,
+
+    Allocator = function()
+        local Class = class()
+        local fixed_instance = {}
+        function Class.__allocator(thisclass)
+            assert(thisclass == Class)
+            return fixed_instance
+        end
+        local instance = Class()
+        assert(instance == fixed_instance)
+        local instance2 = Class()
+        assert(instance2 == fixed_instance)
+    end,
+
+    AllocatorArgs = function()
+        local Class = class()
+        function Class.__allocator(thisclass, t)
+            assert(thisclass == Class)
+            return t
+        end
+        local fixed_instance = {}
+        local instance = Class(fixed_instance)
+        assert(instance == fixed_instance)
+    end,
+
+    AllocatorAndConstructorArgs = function()
+        local Class = class()
+        function Class.__allocator(thisclass, t)
+            assert(thisclass == Class)
+            return t
+        end
+        function Class:__constructor(_, x)
+            self.x = x
+        end
+        local fixed_instance = {}
+        local instance = Class(fixed_instance, 71)
+        assert(instance == fixed_instance)
+        assert(instance.x == 71)
+    end,
+
+    AllocatorMetatable = function()
+        local Class = class()
+        function Class.__allocator(thisclass)
+            assert(thisclass == Class)
+            return setmetatable({}, {foo = 72})
+        end
+        local instance = Class()
+        assert(getmetatable(instance).foo == 72)
+    end,
+
+    AllocatorMetatableIndexTable = function()
+        local Class = class()
+        function Class.__allocator(thisclass)
+            assert(thisclass == Class)
+            return setmetatable({}, {__index = {foo = 73}})
+        end
+        local instance = Class()
+        assert(instance.foo == 73)
+    end,
+
+    AllocatorMetatableIndexFunction = function()
+        local Class = class()
+        function Class.__allocator(thisclass)
+            assert(thisclass == Class)
+            return setmetatable({}, {__index = function(t, k)
+                                         return k=="foo" and 74 or nil
+                                     end})
+        end
+        local instance = Class()
+        assert(instance.foo == 74)
+    end,
+
+    AllocatorMetatableIndexTableFalseValue = function()
+        local Class = class()
+        function Class.__allocator(thisclass)
+            assert(thisclass == Class)
+            return setmetatable({}, {__index = {foo = false}})
+        end
+        local instance = Class()
+        assert(instance.foo == false)
+    end,
+
+    AllocatorMetatableIndexFunctionFalseValue = function()
+        local Class = class()
+        function Class.__allocator(thisclass)
+            assert(thisclass == Class)
+            return setmetatable({}, {__index = function(t, k)
+                                         if k=="foo" then return false end
+                                         return nil
+                                     end})
+        end
+        local instance = Class()
+        assert(instance.foo == false)
+    end,
+
+    AllocatorInvalidResult = function()
+        local Class = class()
+        function Class.__allocator(thisclass)
+            assert(thisclass == Class)
+            return true
+        end
+        local function f() return Class() end
+        local result, errmsg = pcall(f)
+        assert(result == false)
+        assert(errmsg:find(alloc_result_error_msg, 1, true))
+    end,
+
+    InvalidParentType = function()
+        local function f() return class("foo") end
+        local result, errmsg = pcall(f)
+        assert(result == false)
+        assert(errmsg:find(parent_type_error_msg, 1, true))
     end,
 
     InheritConstant = function()
@@ -377,6 +593,34 @@ local tests = {
         assert(errmsg:find(super_error_msg, 1, true))
     end,
 
+    InheritAllocator = function()
+        local Class = class()
+        local fixed_instance = {}
+        function Class:__allocator()
+            return fixed_instance
+        end
+        local SubClass = class(Class)
+        local instance = SubClass()
+        assert(instance == fixed_instance)
+    end,
+
+    OverrideAllocator = function()
+        local Class = class()
+        local fixed_instance = {}
+        function Class:__allocator()
+            return fixed_instance
+        end
+        local SubClass = class(Class)
+        local fixed_instance2 = {}
+        function SubClass:__allocator()
+            return fixed_instance2
+        end
+        local instance = SubClass()
+        assert(instance == fixed_instance2)
+        local instance2 = Class()
+        assert(instance2 == fixed_instance)
+    end,
+
     NestedInherit = function()
         local Class = class()
         Class.CONSTANT = 190
@@ -452,6 +696,108 @@ local tests = {
         local instance3 = Class()
         assert(instance3.x == 230)
         assert(instance3.y == nil)
+    end,
+
+    NestedSuperCall = function()
+        local Class = class()
+        function Class:__constructor()
+            self.x = 240
+        end
+        local SubClass = class(Class)
+        function SubClass:__constructor()
+            self:__super()
+            self.y = 241
+        end
+        local SubSubClass = class(SubClass)
+        function SubSubClass:__constructor()
+            self:__super()
+            self.z = 242
+        end
+        local instance = SubSubClass()
+        assert(instance.x == 240)
+        assert(instance.y == 241)
+        assert(instance.z == 242)
+        local instance2 = SubClass()
+        assert(instance2.x == 240)
+        assert(instance2.y == 241)
+        assert(instance2.z == nil)
+        local instance3 = Class()
+        assert(instance3.x == 240)
+        assert(instance3.y == nil)
+        assert(instance3.z == nil)
+    end,
+
+    NestedInheritAllocator = function()
+        local Class = class()
+        local fixed_instance = {}
+        function Class:__allocator()
+            return fixed_instance
+        end
+        local SubClass = class(Class)
+        local SubSubClass = class(SubClass)
+        local instance = SubSubClass()
+        assert(instance == fixed_instance)
+    end,
+
+    NestedOverrideAllocator = function()
+        local Class = class()
+        local fixed_instance = {}
+        function Class:__allocator()
+            return fixed_instance
+        end
+        local SubClass = class(Class)
+        local SubSubClass = class(Class)
+        local fixed_instance2 = {}
+        function SubSubClass:__allocator()
+            return fixed_instance2
+        end
+        local instance = SubSubClass()
+        assert(instance == fixed_instance2)
+        local instance2 = SubClass()
+        assert(instance2 == fixed_instance)
+        local instance3 = Class()
+        assert(instance3 == fixed_instance)
+    end,
+
+    DeclareSuperMemberAfterSubclass = function()
+        local Class = class()
+        local SubClass = class(Class)
+        Class.x = 250
+        local instance = SubClass()
+        assert(instance.x == 250)
+    end,
+
+    DeclareSuperMemberAfterSubclassInstantiation = function()
+        local Class = class()
+        local SubClass = class(Class)
+        local instance = SubClass()
+        Class.x = 260
+        assert(instance.x == 260)
+    end,
+
+    DeclareSuperConstructorAfterSubclass = function()
+        local Class = class()
+        local SubClass = class(Class)
+        function Class:__constructor()
+            self.x = 270
+        end
+        local instance = SubClass()
+        assert(instance.x == 270)
+    end,
+
+    DeclareSuperConstructorAfterSubclassConstructor = function()
+        local Class = class()
+        local SubClass = class(Class)
+        function SubClass:__constructor()
+            self:__super()
+            self.y = 281
+        end
+        function Class:__constructor()
+            self.x = 280
+        end
+        local instance = SubClass()
+        assert(instance.x == 280)
+        assert(instance.y == 281)
     end,
 }
 
---
+++ _frame.lua
@@ -0,0 +1,90 @@
+--[[
+
+Extends class() from _class.lua to work with WoW Frame instances.
+
+This file implements class()-style classes for the WoW "Frame" type and
+certain derived types, allowing custom classes to inherit from them in
+the same way they would for any class defined with  class().  Instances
+of Frame-derived classes can be passed to WoW API functions just like
+ordinary frames.
+
+
+Create a native-frame-derived class by passing the appropriate type name
+to class(), for example:
+
+    local class = module.class
+    local Frame = module.Frame
+    MyFrame = class(Frame)
+
+Instances of the derived class can then be created as usual:
+
+    instance = MyFrame(...)
+
+Note, however, that because the Lua table representing the instance must
+be created with the CreateFrame() API function, additional arguments to
+CreateFrame() cannot be passed through the usual __super() interface.
+If any such arguments are required, the class must define an
+__allocator() method and pass those arguments to the base class's
+__allocator() method, using the returned value as the created instance.
+When doing this, the base __allocator() should be called as a _static_
+method rather than as a class method:
+
+    MyButton = class(Button)
+    function MyButton.__allocator(thisclass)
+        return Button.__allocator("Button", nil, UIParent,
+                                  "UIPanelButtonTemplate")
+    end
+
+Passing a different frame type as the first CreateFrame() argument
+results in undefined behavior.  (While a bit awkward structurally, this
+allows classes which do not need additional CreateFrame() arguments to
+skip the __allocator() definition even when the constructor accepts
+arguments.  Without this workaround, constructor arguments would be
+passed to CreateFrame(), probably resulting in incorrect behavior.)
+
+
+The following native frame types are supported:
+   - Button
+   - Frame
+
+--]]
+
+local _, module = ...
+assert(module.class, "_class.lua must be loaded first")
+
+
+local SUPPORTED_CLASSES = {"Button", "Frame"}
+
+local function empty_constructor() end
+
+for _, frame_type in ipairs(SUPPORTED_CLASSES) do
+    local frame_class = module.class()
+    local class_metatable = getmetatable(frame_class)  -- Always non-nil.
+
+    -- This is required because we override the class index.
+    class_metatable.__newindex = nil
+
+    -- It's not ideal to create a dummy frame just to get the index,
+    -- since WoW frames stick around in memory forever, but this seems
+    -- to be the simplest option without any way to look up the index
+    -- table directly from the type name.
+    local frame_index = getmetatable(CreateFrame(frame_type)).__index
+    class_metatable.__index = frame_index
+
+    frame_class.__allocator = function(arg1, ...)
+        local instance
+        if arg1 == frame_class then
+            instance = CreateFrame(frame_type)
+        else
+            assert(arg1 == frame_type)
+            instance = CreateFrame(frame_type, ...)
+        end
+        local instance_metatable = getmetatable(instance)
+        assert(instance_metatable.__index == frame_index)
+        -- Suppress the frame's default metatable so we can use normal
+        -- inheritance-based lookup.
+        return setmetatable(instance, nil)
+    end
+
+    module[frame_type] = frame_class
+end
