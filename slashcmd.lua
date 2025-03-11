4963e7a36e2bd2697f6c4b2d6791b0a1596862e0
vaxherd <111935183+vaxherd@users.noreply.github.com>
2025-03-12 03:30:06 +0900

    Add debug command to list races for a map
    See notes at commit fff246da.

--- slashcmd.lua
+++ slashcmd.lua
@@ -1,9 +1,23 @@
 local _, RaceTimes = ...
 RaceTimes.SlashCmd = {}
 
+local strfind = string.find
+local strstr = function(s1,s2,pos) return strfind(s1,s2,pos,true) end
+local strsub = string.sub
+
 SlashCmdHelp = SlashCmdHelp or {}
 
 ------------------------------------------------------------------------
+
+-- Convenience function to list all races in a map.
+local function DumpRaces(map)
+    local races = C_AreaPoiInfo.GetDragonridingRacesForMap(map) or {}
+    for _,race in ipairs(races) do
+        local poi = C_AreaPoiInfo.GetAreaPOIInfo(map, race)
+        print(string.format("%.3f %.3f %s",
+                            poi.position.x*100, poi.position.y*100, poi.name))
+    end
+end
 
 function RaceTimes.SlashCmd.Init()
     SLASH_RACETIMES1 = "/racetimes"
@@ -15,17 +29,23 @@ function RaceTimes.SlashCmd.Init()
                 'With the "settings" subcommand, instead opens the addon settings window.'},
     }
     SlashCmdList["RACETIMES"] = function(arg)
-        if arg == "dump" then
-            RaceTimes.Data.DumpLastTimes()
-            return
-        end
-        if arg == nil or arg == "" then
+        arg = arg or ""
+        local space = strstr(arg, " ") or #arg+1
+        local subcommand = strsub(arg, 1, space-1)
+        arg = strsub(arg, space+1)
+        if subcommand == "" then
             RaceTimes.UI.Open()
-        elseif arg == "recenter" then
+        elseif subcommand == "recenter" then
             RaceTimes.UI.Recenter()
             RaceTimes.UI.Open()
-        elseif arg == "settings" then
+        elseif subcommand == "settings" then
             RaceTimes.Settings.Open()
+        elseif subcommand == "dump" then
+            RaceTimes.Data.DumpLastTimes()
+        elseif subcommand == "dumpraces" then
+            local map = tonumber(arg)
+            if not map then error("Usage: /racetimes dumpraces MAP-ID") end
+            DumpRaces(map)
         end
     end
 end
