50e76413f0b891ce33315611b2093e097aa8f876
vaxherd <111935183+vaxherd@users.noreply.github.com>
2025-08-01 00:34:58 +0900

    Fix obsolete help text

--- slashcmd.lua
+++ slashcmd.lua
@@ -24,7 +24,7 @@ function RaceTimes.SlashCmd.Init()
     SLASH_RACETIMES2 = "/rt"
     SlashCmdHelp["RACETIMES"] = {
         args = "[|cFFFFFF00recenter|r || |cFFFFFF00settings|r]",
-        help = {"Opens the dragon racing personal best time list.",
+        help = {"Opens the skyriding race personal best time list.",
                 'With the "recenter" subcommand, also moves the window back to the center of the screen.',
                 'With the "settings" subcommand, instead opens the addon settings window.'},
     }
