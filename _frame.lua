3d34dbd52dc72770f08c75403676a6662d59efce
vaxherd <111935183+vaxherd@users.noreply.github.com>
2025-07-31 20:54:33 +0900

    Import updated class implementation from WoWXIV

--- _frame.lua
+++ _frame.lua
@@ -4,7 +4,7 @@ Extends class() from _class.lua to work with WoW Frame instances.
 
 This file implements class()-style classes for the WoW "Frame" type and
 certain derived types, allowing custom classes to inherit from them in
-the same way they would for any class defined with  class().  Instances
+the same way they would for any class defined with class().  Instances
 of Frame-derived classes can be passed to WoW API functions just like
 ordinary frames.
 
@@ -22,40 +22,72 @@ Instances of the derived class can then be created as usual:
 
 Note, however, that because the Lua table representing the instance must
 be created with the CreateFrame() API function, additional arguments to
-CreateFrame() cannot be passed through the usual __super() interface.
+CreateFrame() cannot be passed through the usual constructor interface.
 If any such arguments are required, the class must define an
 __allocator() method and pass those arguments to the base class's
 __allocator() method, using the returned value as the created instance.
-When doing this, the base __allocator() should be called as a _static_
-method rather than as a class method:
+When doing this, pass as the first argument to the base __allocator()
+(ordinarily the class object itself) the name of the relevant native
+frame type:
 
     MyButton = class(Button)
     function MyButton.__allocator(thisclass)
-        return Button.__allocator("Button", nil, UIParent,
-                                  "UIPanelButtonTemplate")
+        return __super("Button", nil, UIParent, "UIPanelButtonTemplate")
     end
 
-Passing a different frame type as the first CreateFrame() argument
-results in undefined behavior.  (While a bit awkward structurally, this
-allows classes which do not need additional CreateFrame() arguments to
-skip the __allocator() definition even when the constructor accepts
-arguments.  Without this workaround, constructor arguments would be
-passed to CreateFrame(), probably resulting in incorrect behavior.)
+While a bit awkward structurally, this allows classes which do not need
+additional CreateFrame() arguments to skip the __allocator() definition
+even when the constructor accepts arguments.  Without this workaround,
+constructor arguments would be passed to CreateFrame(), probably
+resulting in incorrect behavior.
 
 
 The following native frame types are supported:
    - Button
    - Frame
 
---]]
+]]--
 
 local _, module = ...
 assert(module.class, "_class.lua must be loaded first")
 
 
-local SUPPORTED_CLASSES = {"Button", "Frame"}
+-- HACK: Ugly workaround for WoW engine limitation.  util.lua is not
+-- loaded at this point, so we have to redeclare this ourselves.
+-- See makefenv() for details.
+local makefenv_hack_names = {
+    "ColorMixin",
+    "ItemLocationMixin",
+    "ItemTransmogInfoMixin",
+    "PlayerLocationMixin",
+    "TransmogLocationMixin",
+    "TransmogPendingInfoMixin",
+    "Vector2DMixin",
+    "Vector3DMixin",
+}
+local wrapped_class = module.class
+local ipairs = ipairs
+local type = type
+module.class = function(...)
+    local classdef = wrapped_class(...)
+    local classmeta = getmetatable(classdef)
+    local methods = classmeta.methods
+    local old_newindex = classmeta.__newindex
+    classmeta.__newindex = function(t, k, v)
+        old_newindex(t, k, v)
+        if type(v) == "function" then
+            local fenv = getfenv(v)
+            for _, name in ipairs(makefenv_hack_names) do
+                -- Must be rawset! (fenv has its own __newindex)
+                rawset(fenv, name, _G[name])
+            end
+        end
+    end
+    return classdef
+end
 
-local function empty_constructor() end
+
+local SUPPORTED_CLASSES = {"Button", "Frame"}
 
 for _, frame_type in ipairs(SUPPORTED_CLASSES) do
     local frame_class = module.class()
@@ -73,7 +105,7 @@ for _, frame_type in ipairs(SUPPORTED_CLASSES) do
 
     frame_class.__allocator = function(arg1, ...)
         local instance
-        if arg1 == frame_class then
+        if type(arg1) == "table" then
             instance = CreateFrame(frame_type)
         else
             assert(arg1 == frame_type)
